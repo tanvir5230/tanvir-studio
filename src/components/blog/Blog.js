400ce50eac7921a5b0ff9e9bebf901fb25d9b66e
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-10-30 23:04:08 +0600

    finished

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -6,6 +6,13 @@ const Blog = () => {
   return (
     <Container fluid className="p-0">
       <Topbar />
+      <h1
+        className="px-5 text-success d-flex justify-content-between border-bottom py-3"
+        style={{ fontSize: "40px" }}
+      >
+        <span>Blog</span>
+        <span className="fa fa-rss"></span>
+      </h1>
       <h1 className="text-success font-weight-bolder text-center text-uppercase">
         Coming soon
       </h1>
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,15 +1,50 @@
 import React from "react";
-import {
-  NavLink,
-  Redirect,
-  Route,
-  Switch,
-  useRouteMatch,
-} from "react-router-dom";
-import { Col, Container, Nav, NavItem, Row } from "reactstrap";
+import { Card, CardBody, CardImg, Col, Container, Row } from "reactstrap";
 import Topbar from "../navigation/Topbar";
+import doctors_portal from "../../photo/projects/doctors-portal.png";
+import creative_agency from "../../photo/projects/creative-agency.png";
+import hot_onion from "../../photo/projects/hot-onion.png";
+import volunteer_network from "../../photo/projects/volunteer-network.png";
+import hot_gadgets from "../../photo/projects/hot_gadgets.PNG";
 
 const Portfolio = () => {
+  const projects = [
+    {
+      image: doctors_portal,
+      title: "doctors-portal",
+      shortDes: "this is a medical service oriented web app.",
+      liveSite: "https://doctors-portal-t.web.app/",
+      github: "https://github.com/tanvir5230/doctor-s-portal-client",
+    },
+    {
+      image: creative_agency,
+      title: "creative-agency",
+      shortDes: "this a technical service provider web app.",
+      liveSite: "https://creative-agency-t.web.app/",
+      github: "https://github.com/tanvir5230/creative-agency-client",
+    },
+    {
+      image: volunteer_network,
+      title: "volunteer-network",
+      shortDes: "this is a voluntary work.",
+      liveSite: "https://volunteer-network-t.web.app/",
+      github: "https://github.com/tanvir5230/volunteer-network-client",
+    },
+    {
+      image: hot_onion,
+      title: "hot-onion",
+      shortDes: "this is a food service web app.",
+      liveSite: "https://hot-onion-adb51.web.app/",
+      github: "https://github.com/tanvir5230/hot-onion",
+    },
+    {
+      image: hot_gadgets,
+      title: "hot-gadgets",
+      shortDes: "this is a e-commerce SITE(view only).",
+      liveSite: "https://tanvir5230.github.io/hot-gadgets/",
+      github: "https://github.com/tanvir5230/hot-gadgets",
+    },
+  ];
   return (
     <Container
       fluid
@@ -24,65 +59,48 @@ const Portfolio = () => {
         <span>Portfolio</span>
         <span className="fa fa-briefcase"></span>
       </h1>
-      <Row>
-        <Col xs={12}>
-          <TabNavs />
-        </Col>
+      <Row className="flex-wrap justify-content-center justify-content-md-start align-items-end px-3">
+        {projects.map((project) => {
+          return (
+            <Col xs={10} md={4} lg={3} className="mt-4">
+              <Card className="shadow">
+                <div
+                  className="overflow-hidden"
+                  style={{ height: "160px", width: "300px" }}
+                >
+                  <CardImg top src={project.image} className="rounded" />
+                </div>
+                <CardBody>
+                  <div className="text-capitalize">
+                    <h5>{project.title}</h5>
+                    <p>{project.shortDes}</p>
+                    <div className="d-flex justify-content-start">
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={project.liveSite}
+                        className="btn btn-outline-warning rounded-pill px-4 font-weight-bold"
+                      >
+                        Live site
+                      </a>
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={project.github}
+                        className="btn btn-outline-primary ml-3 rounded-pill px-4 font-weight-bold"
+                      >
+                        github
+                      </a>
+                    </div>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   );
-};
-
-const TabNavs = () => {
-  const { url, path } = useRouteMatch();
-  return (
-    <>
-      <Nav tabs>
-        <NavItem>
-          <NavLink
-            className="nav-link"
-            activeClassName="active"
-            to={url + "/front-end"}
-          >
-            front end
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to={url + "/react"}>
-            React app
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to={url + "/mern-stack"}>
-            mern stack
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <Switch>
-        <Route path={path + "/front-end"}>
-          <FrontEnd />
-        </Route>
-        <Route path={path + "/react"}>
-          <ReactApp />
-        </Route>
-        <Route path={path + "/mern-stack"}>
-          <MernStack />
-        </Route>
-        <Redirect to={path + "/front-end"} />
-      </Switch>
-    </>
-  );
-};
-
-const FrontEnd = () => {
-  return <h1>hello</h1>;
-};
-
-const MernStack = () => {
-  return <h1>metn stack</h1>;
-};
-const ReactApp = () => {
-  return <h1>resact</h1>;
 };
 
 export default Portfolio;
